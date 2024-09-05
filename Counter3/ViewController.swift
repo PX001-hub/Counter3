820e1d7cf1522497d6f3db1917f1c380f1a40f47
Petr <kod.rf@yandex.ru>
2024-09-05 09:21:23 -0400

    Initial Commit

---
+++ Counter3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter3
+//
+//  Created by P on 05/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
